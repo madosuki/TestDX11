eeecfe25e659258e9991d411f17ac1c79594b707
madosuki <yurikasamaaikatsu@gmail.com>
2020-11-18 18:52:56 +0900

    wip done

--- TestDX11/main.cpp
+++ TestDX11/main.cpp
@@ -1,15 +1,30 @@
 #include <Windows.h>
 #include <iostream>
-#include <d3d11.h>
-#include <d3dcompiler.h>
 #include <wrl/client.h>
 #include <string>
+#include <vector>
+#include <d3d11.h>
+#include <d3dcompiler.h>
+#include <DirectXMath.h>
+
+
 
 
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
 
 class D3DClass
 {
+
+private:
+	ID3D11Device* device_ptr;
+	ID3D11DeviceContext* context_ptr;
+	IDXGISwapChain* swapchain_ptr;
+	ID3D11Texture2D* texture;
+	ID3D11RenderTargetView* render_target_view;
+	D3D11_VIEWPORT viewport;
+
+	HWND hwnd;
+
 public:
 	D3DClass()
 	{
@@ -48,15 +63,70 @@ public:
 
 	HRESULT Init(HWND hWnd);
 
-private:
-	ID3D11Device* device_ptr;
-	ID3D11DeviceContext* context_ptr;
-	IDXGISwapChain* swapchain_ptr;
-	ID3D11Texture2D* texture;
-	ID3D11RenderTargetView* render_target_view;
-	D3D11_VIEWPORT viewport;
+	ID3D11VertexShader* CreateVertexShader(const std::string& filename, const std::string& entrypath = "", bool error = true);
 
-	HWND hwnd;
+	ID3D11PixelShader* CreatePixelShader(const std::string& filename, const std::string& entrypath = "", bool error = true);
+
+	ID3D11InputLayout* CreateInputLayout(D3D11_INPUT_ELEMENT_DESC* layout, UINT elemnt_number, const std::string& filename, const std::string& entrypath = "");
+
+	void SetInputLayout(ID3D11InputLayout* vertex_layout);
+	void SetVertexShader(ID3D11VertexShader* vertex_shader);
+	void SetPixelShader(ID3D11PixelShader* pixel_shader);
+
+	void SetVertexBuffer(ID3D11Buffer* vertex_buffer, UINT buffer_size);
+	void SetIndexBuffer(ID3D11Buffer* index_buffer);
+
+	void SetTexture2D(UINT register_number, ID3D11ShaderResourceView* texture);
+
+	void DrawBegin();
+	void DrawEnd();
+	void DrawIndexed(UINT vertex_number);
+
+
+	template<class x>
+	ID3D11Buffer* CreateVertexBuffer(x* VertexData, UINT VertexNum)
+	{
+		//頂点バッファ作成
+		D3D11_BUFFER_DESC hBufferDesc;
+		ZeroMemory(&hBufferDesc, sizeof(hBufferDesc));
+		hBufferDesc.ByteWidth = sizeof(x) * VertexNum;
+		hBufferDesc.Usage = D3D11_USAGE_DEFAULT;
+		hBufferDesc.BindFlags = D3D11_BIND_VERTEX_BUFFER;
+		hBufferDesc.CPUAccessFlags = 0;
+
+		D3D11_SUBRESOURCE_DATA hSubResourceData;
+		ZeroMemory(&hSubResourceData, sizeof(hSubResourceData));
+		hSubResourceData.pSysMem = VertexData;
+
+		ID3D11Buffer* hpBuffer;
+		if (FAILED(device_ptr->CreateBuffer(&hBufferDesc, &hSubResourceData, &hpBuffer))) {
+			return nullptr;
+		}
+		return hpBuffer;
+	}
+	ID3D11Buffer* CreateIndexBuffer(UINT* Index, UINT IndexNum)
+	{
+		//インデックスバッファ作成
+		D3D11_BUFFER_DESC hBufferDesc;
+		ZeroMemory(&hBufferDesc, sizeof(hBufferDesc));
+		hBufferDesc.ByteWidth = sizeof(UINT) * IndexNum;
+		hBufferDesc.Usage = D3D11_USAGE_DEFAULT;
+		hBufferDesc.BindFlags = D3D11_BIND_INDEX_BUFFER;
+		hBufferDesc.CPUAccessFlags = 0;
+
+		D3D11_SUBRESOURCE_DATA hSubResourceData;
+		ZeroMemory(&hSubResourceData, sizeof(hSubResourceData));
+		hSubResourceData.pSysMem = Index;
+
+		ID3D11Buffer* hpBuffer;
+		if (FAILED(device_ptr->CreateBuffer(&hBufferDesc, &hSubResourceData, &hpBuffer))) {
+			return nullptr;
+		}
+		return hpBuffer;
+	}
+
+
+	
 };
 
 HRESULT D3DClass::Init(HWND hWnd)
@@ -179,6 +249,141 @@ HRESULT D3DClass::Init(HWND hWnd)
 	return hr;
 }
 
+ID3D11VertexShader* D3DClass::CreateVertexShader(const std::string& filename, const std::string& entrypath, bool error)
+{
+	UINT compile_flags = 0;
+
+	setlocale(LC_CTYPE, "jpn");
+	wchar_t ws[512];
+	size_t ws_size;
+	mbstowcs_s(&ws_size, ws, 512, filename.c_str(), _TRUNCATE);
+
+	Microsoft::WRL::ComPtr<ID3DBlob> blob;
+	Microsoft::WRL::ComPtr<ID3DBlob> error_blob;
+
+	HRESULT hr = D3DCompileFromFile(ws, nullptr, D3D_COMPILE_STANDARD_FILE_INCLUDE, entrypath.c_str(), "vs_5_0", compile_flags, 0, &blob, &error_blob);
+	if (FAILED(hr))
+		return nullptr;
+
+	ID3D11VertexShader* vertex_shader;
+	hr = device_ptr->CreateVertexShader(blob->GetBufferPointer(), blob->GetBufferSize(), NULL, &vertex_shader);
+
+	if (FAILED(hr))
+		return nullptr;
+
+	return vertex_shader;
+}
+
+ID3D11PixelShader* D3DClass::CreatePixelShader(const std::string& filename, const std::string& entrypath, bool error)
+{
+	UINT compile_flags = 0;
+
+	Microsoft::WRL::ComPtr<ID3DBlob> blob;
+	Microsoft::WRL::ComPtr<ID3DBlob> blob_error;
+
+	setlocale(LC_CTYPE, "jpn");
+	wchar_t ws[512];
+	size_t ws_size;
+	mbstowcs_s(&ws_size, ws, 512, filename.c_str(), _TRUNCATE);
+
+	auto hr = D3DCompileFromFile(ws, nullptr, D3D_COMPILE_STANDARD_FILE_INCLUDE, entrypath.c_str(), "ps_5_0", compile_flags, 0, &blob, &blob_error);
+	if (FAILED(hr))
+		return nullptr;
+
+	ID3D11PixelShader* pixel_shader;
+	hr = device_ptr->CreatePixelShader(blob->GetBufferPointer(), blob->GetBufferSize(), NULL, &pixel_shader);
+
+	if (FAILED(hr))
+		return nullptr;
+
+	return pixel_shader;
+}
+
+ID3D11InputLayout* D3DClass::CreateInputLayout(D3D11_INPUT_ELEMENT_DESC* layout, UINT element_number, const std::string& filename, const std::string& entrypath)
+{
+	UINT compile_flags = 0;
+
+	Microsoft::WRL::ComPtr<ID3DBlob> blob;
+	Microsoft::WRL::ComPtr<ID3DBlob> blob_error;
+
+	setlocale(LC_CTYPE, "jpn");
+	wchar_t ws[512];
+	size_t ws_size;
+	mbstowcs_s(&ws_size, ws, 512, filename.c_str(), _TRUNCATE);
+
+	auto hr = D3DCompileFromFile(ws, nullptr, D3D_COMPILE_STANDARD_FILE_INCLUDE, entrypath.c_str(), "vs_5_0", compile_flags, 0, &blob, &blob_error);
+	if (FAILED(hr))
+		return nullptr;
+
+	ID3D11InputLayout* vertex_layout_ptr;
+	hr = device_ptr->CreateInputLayout(layout, element_number, blob->GetBufferPointer(), blob->GetBufferSize(), &vertex_layout_ptr);
+
+	if (FAILED(hr))
+		return nullptr;
+
+	return vertex_layout_ptr;
+}
+
+void D3DClass::SetInputLayout(ID3D11InputLayout* vertex_layout)
+{
+	context_ptr->IASetInputLayout(vertex_layout);
+}
+
+void D3DClass::SetVertexShader(ID3D11VertexShader* vertex_shader)
+{
+	context_ptr->VSSetShader(vertex_shader, nullptr, 0);
+}
+
+void D3DClass::SetPixelShader(ID3D11PixelShader* pixel_shader)
+{
+	context_ptr->PSSetShader(pixel_shader, nullptr, 0);
+}
+
+void D3DClass::SetVertexBuffer(ID3D11Buffer* vertex_buffer, UINT buffer_size)
+{
+	UINT hOffsets = 0;
+	context_ptr->IASetVertexBuffers(0, 1, &vertex_buffer, &buffer_size, &hOffsets);
+}
+
+void D3DClass::SetIndexBuffer(ID3D11Buffer* index_buffer)
+{
+	context_ptr->IASetIndexBuffer(index_buffer, DXGI_FORMAT_R32_UINT, 0);
+}
+
+void D3DClass::SetTexture2D(UINT register_number, ID3D11ShaderResourceView* shader_resouce_view)
+{
+	context_ptr->PSSetShaderResources(register_number, 1, &shader_resouce_view);
+}
+
+void D3DClass::DrawBegin()
+{
+	context_ptr->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+
+	// red, green, blue, alpha
+	float clear_color[4] = { 0.0f, 0.0f, 0.0f, 1.0f };
+
+	context_ptr->ClearRenderTargetView(render_target_view, clear_color);
+	context_ptr->RSSetViewports(1, &viewport);
+
+	ID3D11RenderTargetView* rtv[1] = { render_target_view };
+	context_ptr->OMSetRenderTargets(1, rtv, nullptr);
+}
+
+void D3DClass::DrawEnd()
+{
+	swapchain_ptr->Present(0, 0);
+}
+
+void D3DClass::DrawIndexed(UINT vertex_number)
+{
+	context_ptr->DrawIndexed(vertex_number, 0, 0);
+}
+
+struct Vertex
+{
+	DirectX::XMFLOAT3 pos;
+	DirectX::XMFLOAT4 col;
+};
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPreveInstance, LPSTR lpCmdLine, int nCmdShow)
 {
@@ -220,6 +425,48 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPreveInstance, LPSTR lpCmdLin
 	else
 		OutputDebugStringW(L"Success Init\r\n");
 
+
+	std::string shader_path_prefix = "C:\\Users\\madosuki\\Documents\\doc_files\\vs_projects\\TestDX11\\TestDX11\\";
+
+	Microsoft::WRL::ComPtr<ID3D11VertexShader> vertex_shader;
+	vertex_shader.Attach(instance.CreateVertexShader(shader_path_prefix + "VertexShader.hlsl", "main"));
+
+	Microsoft::WRL::ComPtr<ID3D11PixelShader> pixel_shader;
+	pixel_shader.Attach(instance.CreatePixelShader(shader_path_prefix + "PixelShader.hlsl", "main"));
+
+	Microsoft::WRL::ComPtr<ID3D11InputLayout> input_layout;
+	D3D11_INPUT_ELEMENT_DESC elements[] = {
+		{"POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0, D3D11_INPUT_PER_VERTEX_DATA, 0},
+		{"TEXCOORD", 0, DXGI_FORMAT_R32G32B32A32_FLOAT, 0, 12, D3D11_INPUT_PER_VERTEX_DATA, 0}
+	};
+	input_layout.Attach(instance.CreateInputLayout(elements, 2, shader_path_prefix + "VertexShader.hlsl", "main"));
+
+
+	std::vector<Vertex> vertexs =
+	{
+		{ DirectX::XMFLOAT3(-0.5f,-0.5f,0), DirectX::XMFLOAT4(1,0,0,1)},
+		{ DirectX::XMFLOAT3(0.5f,-0.5f,0), DirectX::XMFLOAT4(0,1,0,1)},
+		{ DirectX::XMFLOAT3(0.5f, 0.5f,0), DirectX::XMFLOAT4(0,0,1,1)},
+		{ DirectX::XMFLOAT3(-0.5f, 0.5f,0), DirectX::XMFLOAT4(0,0,0,1)}
+	};
+	Microsoft::WRL::ComPtr<ID3D11Buffer> vb;
+	vb.Attach(instance.CreateVertexBuffer(vertexs.data(), static_cast<UINT>(vertexs.size())));
+
+	std::vector<UINT> idxs = { 0,1,2,0,2,3 };
+	Microsoft::WRL::ComPtr<ID3D11Buffer> ib;
+	ib.Attach(instance.CreateIndexBuffer(idxs.data(), static_cast<UINT>(idxs.size())));
+
+	instance.DrawBegin();
+	instance.SetVertexShader(vertex_shader.Get());
+	instance.SetPixelShader(pixel_shader.Get());
+	instance.SetInputLayout(input_layout.Get());
+	instance.SetVertexBuffer(vb.Get(), sizeof(ID3D11VertexShader));
+	instance.SetIndexBuffer(ib.Get());
+
+	instance.DrawIndexed(static_cast<UINT>(idxs.size()));
+
+	instance.DrawEnd();
+
 	MSG msg = {0};
 
 	while (WM_QUIT != msg.message)
@@ -245,6 +492,6 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 		PostQuitMessage(0);
 		return 0;
 	default:
-		DefWindowProc(hWnd, message, wParam, lParam);
+		return DefWindowProc(hWnd, message, wParam, lParam);
 	}
 }
